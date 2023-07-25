7653b266f1a840c06c41813798be3b278556bf27
Dewank Bawankar <110822108+dewank05@users.noreply.github.com>
2023-07-25 20:54:28 +0530

    first commit

---
+++ javascriptdd.js
@@ -0,0 +1,12 @@
+let color="yellow";
+//voter id decription
+if (color === "red"){
+    console.log("stop!");
+}
+else if(color === "yellow"){
+    console.log("wait for a minute");
+}
+else if(color === "green"){
+    console.log("go");
+}
+
